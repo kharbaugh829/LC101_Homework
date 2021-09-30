b7cf871e17d4bb5e41e28444c1e8223ede4ad720
Kaela's Workstation <mac3@KaelasWkstation.hiteccopy.local>
2021-09-30 15:19:42 -0500

    starting stage for button/links

--- order-form/src/App.js
+++ order-form/src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
-import Home from './components/Home';
+
 import Header from './components/Header';
+
 
 function App() {
   return (
     <div>
       <Header />
-      <Home />
+     
     </div>
   );
 }
--- order-form/src/components/NavigationBar.js
+++ order-form/src/components/NavigationBar.js
@@ -1,12 +1,38 @@
-import React from 'react';
-import Home from './Home';
-import ContactUs from './ContactUs';
-const NavigationBar = () => {
-    return(
-<div><button value ="Home" type = "button" onClick={Home}>Home</button> 
-<button value ="Contact Us" type = "button" onClick={ContactUs}>Contact Us</button></div>
-    )
+import React from "react";
+import Home from "./Home";
+import ContactUs from "./ContactUs";
+const NavigationBar = (props) => {
+    let page = props.page;
+    
+    // const [pageView, setPageView] = useState() => {
+    //     if (button.value === 'Home'); {
+    //         pageView = {Home}
+    //     } else (button.value === 'Contact Us') {
+    //         pageView = {ContactUs}
+    //     };
+    // };
+    const onClickHandler= (props) => {
+       if (props.button.value === "Home"){
+           page = {Home}} else if (props.button.value === "Contact Us") {
+               page = {ContactUs}
+           } else { page = {Home}
+               
+           } 
+        console.log("Clicked!!!")
+        // setPageView({pageSelected})
+    }
 
+  return (
+    <div>
+      <button value="Home" type="button" onClick={onClickHandler()}>
+        Home
+      </button>
+      <button value="Contact Us" type="button" onClick={onClickHandler()}>
+        Contact Us
+      </button>
+      <div>{page}</div>
+    </div>
+  );
 };
 
 export default NavigationBar;
