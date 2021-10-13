0438ba99903211ec1b80ca32a4e31395172ee0eb
Kaela's Workstation <mac3@KaelasWkstation.hiteccopy.local>
2021-10-13 15:01:54 -0500

    Vid 283 Complete

--- 09-time-to-practice-starting-code/src/pages/AllQuotes.js
+++ 09-time-to-practice-starting-code/src/pages/AllQuotes.js
@@ -1,4 +1,13 @@
+import QuoteList from '../components/quotes/QuoteList';
+
+const DUMMY_QUOTES = [
+    { id: 'q1', author: 'Max', text:'Learning React is fun!'},
+    {id: 'q2', author: 'Kaela', text:'Learning React is great!'}
+   
+ 
+];
+
 const AllQuotes =() =>{
-return <h1>All Quotes Page</h1>
+return <QuoteList quotes ={DUMMY_QUOTES}/>
 }
 export default AllQuotes;
